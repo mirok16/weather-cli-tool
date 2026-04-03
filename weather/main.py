d9fe7703ac7afba14ce76a2cb218ca990ee00c2f
mirok16 <154271539+mirok16@users.noreply.github.com>
2026-04-03 22:05:06 +0300

    Add user input support

--- weather/main.py
+++ weather/main.py
@@ -4,3 +4,6 @@ from weather.formatter import format_weather
 if __name__ == "__main__":
     data = get_weather("Berlin")
     print(format_weather(data))
+city = input("Enter city: ")
+data = get_weather(city)
+print(format_weather(data))
