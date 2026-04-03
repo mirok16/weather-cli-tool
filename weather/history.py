e776c0cb4e0bf74a89d5df0a8d21863209c0cdfd
mirok16 <154271539+mirok16@users.noreply.github.com>
2026-04-03 22:03:49 +0300

    Integrate API with history

--- weather/history.py
+++ weather/history.py
@@ -4,3 +4,10 @@ def add_to_history(record):
     history.append(record)
 def get_history():
     return history
+from weather.api import get_weather
+from weather.history import add_to_history
+
+def fetch(city):
+    data = get_weather(city)
+    add_to_history(data)
+    return data
